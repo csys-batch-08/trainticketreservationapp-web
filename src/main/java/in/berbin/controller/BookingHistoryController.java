ea18071b54aaf9466e688f95ffc9092432527e96
Berbin jose <berbinjoseberbin@gmail.com>
2022-02-07 17:42:25 +0530

    changes in jsp

--- src/main/java/in/berbin/controller/BookingHistoryController.java
+++ src/main/java/in/berbin/controller/BookingHistoryController.java
@@ -21,17 +21,21 @@ public class BookingHistoryController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	 
-	     HttpSession session=request.getSession();
+	     try {
+			HttpSession session=request.getSession();
    Users userData=(Users) session.getAttribute("userdata");
-    BookingDetailsDaoImpl bookingDetailsDao=new BookingDetailsDaoImpl();
-    List<BookingDetails> bookingDetailsModel=bookingDetailsDao.getBookingDetailsForPresentUser(userData);
-    for (int i=1; i<bookingDetailsModel.size(); i++) {  	
-   	BookingDetails book = bookingDetailsModel.get(i);   	  
-   	}
+   BookingDetailsDaoImpl bookingDetailsDao=new BookingDetailsDaoImpl();
+   List<BookingDetails> bookingDetailsModel=bookingDetailsDao.getBookingDetailsForPresentUser(userData);
+   for (int i=1; i<bookingDetailsModel.size(); i++) {  	
+  	BookingDetails book = bookingDetailsModel.get(i);   	  
+  	}
    session.setAttribute("currentttt", bookingDetailsModel);
    //response.sendRedirect("bookingHistory.jsp");
    RequestDispatcher req = request.getRequestDispatcher("bookingHistory.jsp");
-		req.forward(request, response);
+			req.forward(request, response);
+		} catch (ServletException | IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
